8812ce6584395bc9e9c3aa0257bd3c2594d9ddf6
SangHyun Jung <wjdtkdgus876@naver.com>
2025-07-08 18:11:23 +0900

    feat: 강의(8) 수강, modal visibible 과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,10 @@ function App() {
   // headline[index]로 접근 
   // let의 두번째 인자 -> state 변경함수 
   let [like, changeLike] = useState(0);
+  let [display, setDisplay] = useState(false);
 
-
+  // return 내부에는 하나의 tag만 존재 
+  // -> <></> (fragment 문법) or <div></div> 하나의 태그만 존재하도록 
   return (
     // JSX로 jsx파일에서 html작성 
     <div className="App">
@@ -54,12 +56,33 @@ function App() {
       </div>
 
       <div className='list'>
-        <h4>{headline[2]}</h4>
+        <h4 onClick={() => setDisplay(!display)}>{headline[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
 
+      {
+        display == true ? <Modal></Modal> : null
+      }
     </div >
   );
 }
+
+// Component : 축약한 HTML 묶음 
+/* 
+1. 반복해서 출현하는 HTML 묶음
+2. 내용이 자주 변경되는 부분
+3. 다른 페이지에 등장
+4. 팀원과 협업시에 작업 분배단위
+*/
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
+
 
 export default App
