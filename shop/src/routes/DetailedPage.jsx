81bfae330ffd570f6f64803438b5f0c809628cf3
SangHyun Jung <wjdtkdgus876@naver.com>
2025-07-24 20:51:44 +0900

    feat: 강의(11) 수강 -> input 타입 체크 후 alert 과제

--- shop/src/routes/DetailedPage.jsx
+++ shop/src/routes/DetailedPage.jsx
@@ -23,9 +23,12 @@ function DetailedPage(props) {
 
     // useEffect()
     // 컴포넌트가 mount / update할때 수행 (dependency 없을 때)
-    // html '렌더링 이후'에 수행 
+    // html '렌더링 이후'에 수행
     // 1. 어려운 연산 / 2. 서버에서 데이터 가져오기 / 3. 타이머 장착
     // 목적 : 핵심기능이 아닌 다른 기능들을 넣는다
+
+    let [popup, setPopup] = useState(true);
+    let [isNum, setIsNum] = useState('');
 
     useEffect(() => {
         let timer = setTimeout(() => { // 2초 후 시행 할 로직
@@ -33,18 +36,19 @@ function DetailedPage(props) {
         }, 2000);
         console.log(2);
 
+        if (isNaN(isNum)) {
+            alert('숫자만 입력하세요');
+        }
         // clean up function : useEffect 실행 전에 실행 (unmount할때도 실행)
         // 주요 용도 : 타이머 제거, socket 연결 제거, ajax 요청 중단 
         return () => {
             console.log(1)
             clearTimeout(timer);
         }
-    }, []); // dependency를 []로 하면 mount될때만 실행
+    },[isNum]); // dependency를 []로 하면 mount될때만 실행
 
     let { id } = useParams(); // 구조분해 
     let shoe = props.shoes.find(shoe => shoe.id === Number(id)); // === : type비교 
-
-    let [popup, setPopup] = useState(true);
 
     if (!shoe) {
         return (
@@ -68,7 +72,16 @@ function DetailedPage(props) {
                         <h4 className="pt-5">{shoe.title}</h4>
                         <p>{shoe.content}</p>
                         <p>{shoe.price}</p>
+
+                        <div>
+                            <input type="text" placeholder='숫자만 입력하세요' onChange={(e) => {
+                                setIsNum(e.target.value);
+
+                            }}></input>
+                        </div>
+
                         <button className="btn btn-danger">주문하기</button>
+                        
                     </div>
                 </div>
             </div>
