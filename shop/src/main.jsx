94de7afa7a9dacd6d20bf9f8e61627c60f01a2d9
SangHyun Jung <wjdtkdgus876@naver.com>
2025-08-03 15:37:28 +0900

    feat: react-query 세팅 설명추가, StrictMode 태그 적용

--- shop/src/main.jsx
+++ shop/src/main.jsx
@@ -7,6 +7,8 @@ import store from './redux/store.js'
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+// React Query의 중앙 상태 관리자
+// queryClient내부 : 캐시된 데이터, 쿼리 상태(isLoading, isError, data), 쿼리 invalidation 로직
 const queryClient = new QueryClient();
 
 // id = "root"인 div찾고 App그리기 (render)
@@ -17,16 +19,18 @@ createRoot(document.getElementById('root')).render(
   //   </BrowserRouter>
   // </React.StrictMode>
 
-  // Redux store를 앱 전체에 연결 -> 전역상태에서 관리
+  <StrictMode>
+    {/* React context 기반 모든 하위 컴포넌트에서 useQuery, useMutation등을 사용  */}
+    <QueryClientProvider client={queryClient}>
 
-  <QueryClientProvider client = {queryClient}>
-    <Provider store = {store}>
-      {/* URL에 따라 화면이 바뀌도록 */}
-      <BrowserRouter>
-        <App></App>
-      </BrowserRouter>
-  </Provider>
-  </QueryClientProvider>
-  
+      {/* /Redux store를 앱 전체에 연결 -> 전역상태에서 관리 */}
+      <Provider store = {store}>
+        {/* URL에 따라 화면이 바뀌도록 */}
+        <BrowserRouter>
+          <App></App>
+        </BrowserRouter>
+    </Provider>
+    </QueryClientProvider>
+  </StrictMode>
 
 )
